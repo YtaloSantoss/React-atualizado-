bf04cd8c805e46725d5768b2f9c4db874241df98
Ytalo Mateus <99446046+YtaloSantoss@users.noreply.github.com>
2023-05-31 18:45:17 -0300

    atlz

---
+++ fundamentos-do-react/react-exercicio-4/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+
+function App(){
+  const [password, setPassword] = useState("")
+  const [copyText, setCopyText] = useState("Copiar")
+
+  function generate() {
+    const characters =  "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
+    const length = 12
+    let newPassword= ""
+    for (let i = 0; i < length; i++) {
+      const position = Math.floor(Math.random() * characters.length)
+      newPassword += characters[position]
+    }
+    setPassword(newPassword)
+    setCopyText("Copiar")
+  }
+    function copyToClipboard(){
+      window.navigator.clipboard.writeText(password)
+      setCopyText("Copiado!")
+    }
+  return (
+    <div className="app">
+      <h1>Gerador de senhas</h1>
+      <button onClick={generate}>Gerar!</button>
+      <button onClick={copyToClipboard}>{copyText}</button>
+      <div>{password}</div>
+    </div>
+  )
+}
+
+export default App
