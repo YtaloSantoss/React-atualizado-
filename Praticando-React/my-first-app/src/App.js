4c7df46cd3972a6bd91a5c181f3e939f4ccc8d14
Ytalo Mateus <99446046+YtaloSantoss@users.noreply.github.com>
2023-06-03 17:28:12 -0300

    atlz

---
+++ Praticando-React/my-first-app/src/App.js
@@ -0,0 +1,16 @@
+import { React } from 'react';
+
+function HelloWorld() {
+  return <h1>Hello World</h1>
+}
+
+
+function App() {
+  return (
+  <div>
+    <HelloWorld/>
+  </div>
+  );
+}
+
+export default App;
