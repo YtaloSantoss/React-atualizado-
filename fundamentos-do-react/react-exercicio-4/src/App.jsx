d6be621d0d5b2c0ddd2ea2431e146a8eb0b589b6
Ytalo Mateus <99446046+YtaloSantoss@users.noreply.github.com>
2023-06-06 17:54:52 -0300

    atlz

--- fundamentos-do-react/react-exercicio-4/src/App.jsx
+++ fundamentos-do-react/react-exercicio-4/src/App.jsx
@@ -4,12 +4,12 @@ import { useState } from "react"
 function App(){
   const [password, setPassword] = useState("")
   const [copyText, setCopyText] = useState("Copiar")
+  const [passwordSize, setPasswordSize] = useState(12)
 
   function generate() {
     const characters =  "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
-    const length = 12
     let newPassword= ""
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < passwordSize; i++) {
       const position = Math.floor(Math.random() * characters.length)
       newPassword += characters[position]
     }
@@ -23,7 +23,17 @@ function App(){
   return (
     <div className="app">
       <h1>Gerador de senhas</h1>
-      <button onClick={generate}>Gerar!</button>
+      <div>
+        <label htlmFor="passwordSize">Tamanho:</label>
+        <input 
+        type="number" 
+        id="passwordSize" 
+        min={1}
+        value={passwordSize}
+        onChange={(ev) => setPasswordSize(ev.target.value)}
+        />
+      </div>
+      <button onClick={generate}>Gerar senha de {passwordSize} characters!</button>
       <button onClick={copyToClipboard}>{copyText}</button>
       <div>{password}</div>
     </div>
