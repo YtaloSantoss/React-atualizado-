386b64ca2d9a4ebb81914181611cd4c4a076b8ac
Ytalo Mateus <99446046+YtaloSantoss@users.noreply.github.com>
2023-07-25 16:14:04 -0300

    atlz

--- Exercicios-React/pratica-02/src/components/Card/index.jsx
+++ Exercicios-React/pratica-02/src/components/Card/index.jsx
@@ -1,14 +1,15 @@
 import posterImg from "../../assets/imageee.jpg"
 import styles from "./styles.module.css"
 
+
 export default function Card() {
   return (
     <div className={styles.container}>
-      <img src={posterImg} alt="" />
+      <img style={{ width: "300px", height: "auto", }} className={styles.poster} src={posterImg} alt="" />
       <div>
-        <h2></h2>
-        <p></p>
-        <button></button>
+        <h2>Quadro Pintado</h2>
+        <p>Um quadro pintador por Artista desconhecido</p>
+        <button>Compre agora</button>
       </div>
     </div>
   )
