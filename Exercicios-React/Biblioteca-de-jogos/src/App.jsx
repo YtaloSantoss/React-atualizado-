beab701c1d263d8457050076e47f84806729b0d9
Ytalo Mateus <99446046+YtaloSantoss@users.noreply.github.com>
2023-07-29 16:09:52 -0300

    atlz

--- Exercicios-React/Biblioteca-de-jogos/src/App.jsx
+++ Exercicios-React/Biblioteca-de-jogos/src/App.jsx
@@ -1,21 +1,44 @@
-
-
+import { useState } from "react"
 
 
 
 function App() {
+  const [title, setTitle] = useState("")
+  const [cover, setCover] = useState("")
+
+  const handleSubmit = (ev) => {
+    ev.preventDault()
+  }
+
 
   return (
     <div id="app">
-      <label htmlFor="title">Title: </label>
-      <input type="text" name="title" id="title" />
+      <h1>Biblioteca de Jogos</h1>
+      <form action="">
+        <div>
+          <label htmlFor="title">Title: </label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="cover">Capa: </label>
+          <input
+            type="text"
+            name="cover"
+            id="cover"
+            onChange={(e) => setCover(e.target.value)}
+          />
+        </div>
+        <button type="submit">Adicionar à biblioteca</button>
+      </form>
     </div>
-    <div>
-      <label htmlFor="cover">Capa: </label>
-      <input type="text" name="cover" id="cover" />
-    </div>
-    <button type="submit">Adicionar à biblioteca</button>
   )
+
 }
 
 export default App
