d9dbee637de4c416fee3674d3f8ca5d9dc48c0a0
Ytalo Mateus <99446046+YtaloSantoss@users.noreply.github.com>
2023-07-29 18:27:58 -0300

    atlz

--- Exercicios-React/Biblioteca-de-jogos/src/App.jsx
+++ Exercicios-React/Biblioteca-de-jogos/src/App.jsx
@@ -3,18 +3,51 @@ import { useState } from "react"
 
 
 function App() {
+  const [games, setGames] = useState(() => {
+    const storedGames = localStorage.getItem("obc-game-lib")
+    if (!storedGames) return []
+    const gameArray = JSON.parse(storedGames)
+    return gameArray
+
+  })
   const [title, setTitle] = useState("")
   const [cover, setCover] = useState("")
 
-  const handleSubmit = (ev) => {
-    ev.preventDault()
+
+  const addGame = ({ title, cover }) => {
+    const id = Math.floor(Math.random() * 1000000)
+    const game = { id, title, cover }
+
+    setGames(state => {
+      const newState = [...state, game]
+      localStorage.setItem("obc-game-lib", JSON.stringify(newState))
+      return newState
+    })
   }
+
+  const removeGame = (id) => {
+    setGames(state => {
+      const newState = state.filter(game => game.id !== id)
+      localStorage.setItem("obc-game-lib", JSON.stringify(newState))
+      return newState
+    })
+
+  }
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault()
+    addGame({ title, cover })
+    setTitle("")
+    setCover("")
+
+  }
+
 
 
   return (
     <div id="app">
       <h1>Biblioteca de Jogos</h1>
-      <form action="">
+      <form onSubmit={handleSubmit} >
         <div>
           <label htmlFor="title">Title: </label>
           <input
@@ -31,11 +64,23 @@ function App() {
             type="text"
             name="cover"
             id="cover"
+            value={cover}
             onChange={(e) => setCover(e.target.value)}
           />
         </div>
         <button type="submit">Adicionar à biblioteca</button>
       </form>
+      <div className="games">
+        {games.map((game) => (
+          <div key={game.id}>
+            <img src={game.cover} alt="" />
+            <div>
+              <h2>{game.title}</h2>
+              <button onClick={() => removeGame(game.id)}>Remover</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 
