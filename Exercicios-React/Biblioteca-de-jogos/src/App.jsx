ea5dbd37dabd2815ed5e86dc16c8b49a71961004
Ytalo Mateus <99446046+YtaloSantoss@users.noreply.github.com>
2023-07-29 15:51:42 -0300

    atlz

--- Exercicios-React/Biblioteca-de-jogos/src/App.jsx
+++ Exercicios-React/Biblioteca-de-jogos/src/App.jsx
@@ -6,7 +6,15 @@
 function App() {
 
   return (
-    
+    <div id="app">
+      <label htmlFor="title">Title: </label>
+      <input type="text" name="title" id="title" />
+    </div>
+    <div>
+      <label htmlFor="cover">Capa: </label>
+      <input type="text" name="cover" id="cover" />
+    </div>
+    <button type="submit">Adicionar à biblioteca</button>
   )
 }
 
