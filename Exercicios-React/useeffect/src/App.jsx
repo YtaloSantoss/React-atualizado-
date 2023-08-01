20865c13fee056d3975cf3f3e4ce8a1696ed5c57
Ytalo Mateus <99446046+YtaloSantoss@users.noreply.github.com>
2023-08-01 19:59:30 -0300

    atlz

--- Exercicios-React/useeffect/src/App.jsx
+++ Exercicios-React/useeffect/src/App.jsx
@@ -1,18 +1,44 @@
 import { useEffect, useState } from "react"
 
-
-function App() {
+function Counter() {
   const [counter, setCounter] = useState(0)
 
   useEffect(() => {
-    alert("O efeito coloteral foi ativado")
+    console.log("O efeito colateral foi ativado!")
+    return () => {
+      console.log("Fazendo a limpeza componente desmontado")
+      console.log("Componente desmontado! Fazendo a limpeza...")
+    }
   }, [])
+
+  return (
+    <button onClick={() => setCounter(count => count + 1)}>
+      Contador: {counter}
+    </button>
+  )
+}
+
+
+function App() {
+  const [show, setShow] = useState(false)
+
   return (
     <div>
       <h1>Conhecendo useEffect</h1>
-      <button onClick={() => setCounter(count => count + 1)}>
-        Contador:{counter}
-      </button>
+      <div>
+        <input
+          type="checkbox"
+          id="show"
+          value={show}
+          onChange={() => setShow(state => !state)}
+        />
+        <label htmlFor="show">Exibir</label>
+      </div>
+      {
+        show ? <Counter /> : null
+      }
+
+      <hr />
     </div>
   )
 }
