607f7733d34b59f11e5231dbd944091a986ddce6
Ytalo Mateus <99446046+YtaloSantoss@users.noreply.github.com>
2023-08-01 16:25:54 -0300

    atlz

--- Exercicios-React/useref/src/App.jsx
+++ Exercicios-React/useref/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
+import RefExample from "./RefExample";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const variable = 0
+  const ref = useRef(0);
+  const [state, setState] = useState(0);
+
+  const showValues = () => {
+    alert(`
+      Variável: ${variable}
+      Ref: ${ref.current}
+      State: ${state}
+    `)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Exemplo de useRef com o DOM</h2>
+      <RefExample />
+      <hr />
+      <h2>Conhecendo o useRef</h2>
+      <p>Variável: {variable}</p>
+      <p>Ref: {ref.current}</p>
+      <p>State: {state}</p>
+      <button onClick={() => variable++}>Aumentar Variável</button>
+      <button onClick={() => ref.current++}>Aumentar Ref</button>
+      <button onClick={() => setState(state => state + 1)}>Aumentar State</button>
+      <button onClick={showValues}>Exibir valores</button>
+      <hr />
+
+    </div>
   )
 }
 
